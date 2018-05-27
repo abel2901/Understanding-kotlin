e849d43eecd2531a344377d34b8c218e2f59e9c9
abelaraujo <abel2901@gmail.com>
2018-05-27 17:16:37 -0300

    Adding event to button

--- KotlinExampleMyTrip/app/src/main/java/com/example/android/mytrip/MainActivity.kt
+++ KotlinExampleMyTrip/app/src/main/java/com/example/android/mytrip/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.android.mytrip
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.buttonCalculate) {
+            handleCalculate()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonCalculate.setOnClickListener(this)
+    }
+
+    private fun handleCalculate() {
+
     }
 }
